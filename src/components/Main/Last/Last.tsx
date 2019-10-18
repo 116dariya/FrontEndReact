d01f1743d12244c345b0f14ed965e6f9ea1ecf3d
Elibay <elibay98@gmail.com>
2019-10-18 20:31:00 +0600

    fixing

--- src/components/Main/Last/Last.tsx
+++ src/components/Main/Last/Last.tsx
@@ -5,14 +5,20 @@ export class Last extends React.Component {
     render() {
         var items = [];
         let url = "https://assets.vogue.com/photos/5da795d3101aa4000920e07c/1:1/w_240,c_limit/simz-2-promo.jpg";
-        let text = "Little Simz Breaks All the Red Carpet Suiting Rules";
+        let title = "Little Simz Breaks All the Red Carpet Suiting Rules";
         let author = "RACHEL HAHN";
+        let date = "2 days ago";
         for (let i = 1; i <= 4; ++ i) {
             items.push(
                 <div className="last-items-detail">
-                    <img src={url} className="popular-image" alt="Kek"/>
-                    <a href="#">{text}</a>
-                    <a href="#">{author}</a>
+                    <div className="feed-card">
+                        <img src={url} className="popular-image" alt="Kek"/>
+                        <h2 className="feed-card--title">
+                            <a href="#">{title}</a>
+                        </h2>
+
+                        <a href="#">{author}</a>
+                    </div>
                 </div>
             );
         }
